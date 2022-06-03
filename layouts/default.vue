1ac9cee2351c75fb976227da65f69476e99e6455
Alexis Germain (ACF) <Alexis_Germain@acg.corpnet.asus>
2022-06-03 12:00:37 +0200

    1.0.0

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <transition name="page" mode="out-in">
+      <Nuxt />
+    </transition>
+  </v-app>
+</template>
+
+<script>
+import aosMixin from '~/mixins/aos'
+export default {
+  mixins: [aosMixin],
+  transition: {
+    name: 'home',
+    mode: 'out-in',
+  },
+}
+</script>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+html {
+  overflow-x: hidden !important;
+}
+</style>
